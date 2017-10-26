06ba80589de937ab60688aec2c7aceab44a34439
Kelly Selden <kellyselden@gmail.com>
2017-10-25 18:52:13 -0700

    update ember-cli 2.16.2

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,7 +1,8 @@
-import Ember from 'ember';
+import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
 
-export default Ember.Controller.extend({
-  bar: Ember.inject.service(),
+export default Controller.extend({
+  bar: service(),
 
   setup(model) {
     this.setProperties(model);
